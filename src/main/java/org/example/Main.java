743c4b110c8ef11d58b39197f9e331bc75051162
assiph <asif857@gmail.com>
2022-11-23 19:24:23 +0200

    finished Worker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         WorkerClass work = new WorkerClass();
         while (true) {
+            System.out.println("works");
             Message message = work.getFromManagerToWorkerSQS();
             if (message != null) {
                 work.updateFromMessage(message);
