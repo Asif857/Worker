135c93ddcf917bcc68882230dda0eaef6ed2947b
assiph <asif857@gmail.com>
2022-12-01 11:59:58 +0200

    finished Worker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import com.amazonaws.services.sqs.model.Message;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+   public static void main(String[] args) throws Exception {
         WorkerClass work = new WorkerClass();
         while (true) {
             Message message = work.getFromManagerToWorkerSQS();
