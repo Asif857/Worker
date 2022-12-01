b6020eb0158dd9ec5bbc86a26e5f37a33bd51420
assiph <asif857@gmail.com>
2022-12-01 12:49:36 +0200

    finished Worker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
 import com.amazonaws.services.sqs.model.Message;
+import net.sourceforge.tess4j.Tesseract;
+
+import java.io.File;
 
 public class Main {
    public static void main(String[] args) throws Exception {
-        WorkerClass work = new WorkerClass();
+        /*WorkerClass work = new WorkerClass();
         while (true) {
             Message message = work.getFromManagerToWorkerSQS();
             if (message != null) {
@@ -14,5 +17,14 @@ public class Main {
                 work.deleteMessage(message);
             }
         }
+
+         */
+       Tesseract tesseract = new Tesseract();
+       tesseract.setDatapath("/home/assiph/Desktop/key/tessdata");
+       String imageProcessedText = tesseract.doOCR(new File("/home/assiph/IdeaProjects/Worker/src/main/Images/ocraextended.png"));
+       System.out.println(imageProcessedText);
+
+
     }
+
 }
